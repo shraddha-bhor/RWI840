6f61ed4e8dbc69e1a72014ac157bc83f0a686e10
shraddha-bhor <103180049+shraddha-bhor@users.noreply.github.com>
2024-05-24 09:59:44 +0530

    Add files via upload

---
+++ example.java
@@ -0,0 +1,14 @@
+public class example {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("this is main block ");
+	}
+	
+	static{
+		int a =10;
+		System.out.println("this is static block and value of " +a);
+		
+	}
+	
+	}
