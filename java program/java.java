a2cb3efda28d8ebc341deae95e41aa3fe6818ce9
shraddha-bhor <103180049+shraddha-bhor@users.noreply.github.com>
2024-05-24 10:15:06 +0530

    Add files via upload

---
+++ java program/java.java
@@ -0,0 +1,18 @@
+
+public class java {
+
+	
+    public static void main(String[] args) {
+       
+        
+        System.out.println("==main method==");
+   }
+    
+    static {
+        
+        int a =5;
+        int b=10;
+        
+        System.out.println("Addition of two number " +(a+b));
+    }
+}
