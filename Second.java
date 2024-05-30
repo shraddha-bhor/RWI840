b4e9d123dbb42424d484972c1bc212050470056c
shraddha-bhor <103180049+shraddha-bhor@users.noreply.github.com>
2024-05-30 17:18:10 +0530

    Add files via upload

---
+++ Second.java
@@ -0,0 +1,14 @@
+
+
+    public class Second{
+	
+        public static void main(String[] args) {
+            
+            main ma = new main();
+            ma.setId(12);
+            ma.setName("Shraddha");
+            System.out.println(ma.getId());
+            System.out.println(ma.getName());
+        }
+
+}
