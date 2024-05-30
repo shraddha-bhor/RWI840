6cdc15a82156c5957ee3adce870c4ddaac909dc0
shraddha-bhor <103180049+shraddha-bhor@users.noreply.github.com>
2024-05-30 18:09:02 +0530

    Add files via upload

---
+++ MainC.java
@@ -0,0 +1,17 @@
+
+
+    abstract class MainC {
+	
+	
+        public abstract void Add();
+        
+        
+        public void print(){
+            
+            System.out.println("****");
+            
+        }
+              
+    
+    }
+
